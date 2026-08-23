5e8ae19003eb3e2d95bf5e423a185778a40598be
Bluewick <vivekdangare26@gmail.com>
2026-08-23 22:50:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <h2 className='text-2xl text-black bg-blue-600'>Cloud Storage</h2>
+    </section>   
+        
+    </>
+  )
+}
+
+export default App
